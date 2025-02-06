1dca9bcdf6f5e86d8c6334606ee397581f72ced9
marina <huber.marina@gmail.com>
2025-02-06 14:09:45 +0100

    update snapshot tests

--- MobileShopTests/Application/Cart/CartSnapshotTests.swift
+++ MobileShopTests/Application/Cart/CartSnapshotTests.swift
@@ -10,13 +10,18 @@ import SwiftUI
 
 class CartSnapshotTests: XCTestCase {
 
+    override func setUp() {
+        super.setUp()
+        SnapshotTesting.diffTool = "ksdiff"
+    }
+
     func testSwiftUICartViewFullWithProducts() {
         let cartView = CartItemView(viewModel: CartItemListViewAdapter(content: .init(products: [
             CartProductItemViewContent(title: "Cotton T-shirt", price: "$19.99", quantity: "x2", discount: nil),
             CartProductItemViewContent(title: "Baseball cap", price: "$21.99", quantity: "x1", discount: "5% off"),
             CartProductItemViewContent(title: "Premium T-shirt", price: "$29.99", quantity: "x1", discount: "10% off"),
             CartProductItemViewContent(title: "Coupon discount", price: "", quantity: "", discount: "5% off")
-        ], discounts: [], total: .init(title: "Total", amount: "$85")), payTitle: "Pay"), showPayView: true)
+        ], discounts: [], total: .init(title: "Total", amount: "$85")), payTitle: "Pay"), showPayView: true).referenceFrame()
 
         assertSnapshot(matching: cartView, as: .image)
     }
